88d86956583a714e2f1c706f03b2594162074439
Justin Sung <jjs233@justins-macbook-air.local>
2018-11-25 20:23:26 -0500

    Initial Commit

---
+++ Salamanders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Salamanders
+//
+//  Created by Justin Sung on 11/25/18.
+//  Copyright © 2018 Justin Sung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
